fa8931bd6ad9a8c81afaed51a34a47710e6f764e
Nayemul <nayemulhasan2016@gmail.com>
2021-06-15 02:37:03 +0600

    tailwindcss and routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import ProductDetails from './components/ProductDetails';
+import ProductsList from './components/Products/ProductsList';
+import Error404 from './error404/Error404';
 function App() {
   return (
     <div className="grid-container">
-      <header>
-        shopping cart
-      </header>
-      <main>
-        product
-      </main>
+      <Router>
+        <header>
+          shopping cart
+        </header>
+        <Switch>
+          <main>
+           <Route path="/" exact component={ProductsList}/>
+           <Route path="product/:productId" component={ProductDetails}/>
+           <Route path="*" component={Error404} />
+          </main>
+        </Switch>
+      </Router>
       <footer>
         All reserved.
       </footer>
