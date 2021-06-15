604b66fd84823763c37ffee55fd18dbcb7434ad3
Nayemul <nayemulhasan2016@gmail.com>
2021-06-16 04:27:47 +0600

    final

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,9 +1,46 @@
+import axios from 'axios';
 import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { removeSelectedProduct, selectedProduct } from '../redux/actions/productAction';
 
 const ProductDetails = () => {
+    const product = useSelector(state => state.product)
+    const {image, title, description, category, price} = product;
+    const {productId} = useParams();
+    console.log(product);
+    const dispatch = useDispatch();
+    const getProductDetails = async()=>{
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
+        .catch((err)=>{
+            console.log("Err",err)
+        })
+        dispatch(selectedProduct(response.data))
+    }
+    useEffect(()=>{
+        if(productId && productId !== "") getProductDetails()
+        dispatch(removeSelectedProduct())
+    },[productId]);
     return (
-        <div>
-            <h3>product details</h3>
+        <div className="max-w-md mx-auto bg-gray-200 rounded-xl shadow-md overflow-hidden md:max-w-7xl">
+            {Object.keys(product).length === 0? (
+                <div>Loading...</div>
+            ):(
+                <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                 <img className="w-full md:h-full md:w-80" src={image} alt={title} />
+                </div>
+                <div className="p-8">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{category}</div>
+                <a href="#" className="block mt-1 text-xl leading-tight font-medium text-black hover:underline">{title}</a>
+                <p className="mt-2 text-yellow-500 text-3xl">${price}</p>
+                <p className="mt-2 text-gray-500">{description}</p>
+                <button className="btn">Add to Cart</button>
+                </div>
+            </div> 
+            )}
+            
         </div>
     )
 }
